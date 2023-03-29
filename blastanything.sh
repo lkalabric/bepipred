398c152efa18ffedee75f9fc2ece236ffead0c98
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-03-29 13:29:11 -0300

    Update blastanything.sh

--- blastanything.sh
+++ blastanything.sh
@@ -50,8 +50,8 @@ function blastanything () {
   echo -e "Classificando as reads pelo ${BLASTSUITE}...\n"
   if [ -f ${QUERY} ]; then
 	# Cria o comando Blast suite para busca em banco de sequencias local
-	QUERYNAME=echo$(basename ${QUERY})
-      	CALL_FUNC=echo$(${BLASTSUITE} -db "${BLASTDBDIR}refseq" -query "${QUERY}" -out "${BLASTRESULTSDIR}/${QUERYNAME}.${BLASTSUITE}" -outfmt "6 qseqid length mismatch gapopen qstart qend sstart send evalue qcovhsp" -qcov_hsp_perc ${QCOV} -max_target_seqs 1)
+	QUERYNAME=echo $(basename ${QUERY})
+      	CALL_FUNC=echo $(${BLASTSUITE} -db "${BLASTDBDIR}refseq" -query "${QUERY}" -out "${BLASTRESULTSDIR}/${QUERYNAME}.${BLASTSUITE}" -outfmt "6 qseqid length mismatch gapopen qstart qend sstart send evalue qcovhsp" -qcov_hsp_perc ${QCOV} -max_target_seqs 1)
       	# Executa o comando contido na variável CALL_FUNC
       	eval $CALL_FUNC 
       	# Gera o arquivo de log
